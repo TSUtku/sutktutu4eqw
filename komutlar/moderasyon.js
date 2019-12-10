265ebc1b5f6fc7eb1ce5961da6b536e3d48947a7
Glitch (hello-express) <none>
2019-12-10 22:24:19 +0000

    👍🙈 Checkpoint ./komutlar/yardımkomut.js:5958247/1592 ./watch.json:5958247/14 ./komutlar/moderasyon.js:5958247/2008

--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -2,18 +2,19 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 exports.run = (client, message, args) => {
-  
+
     const juke = new Discord.RichEmbed()
       .setColor('BLUE')
       .setAuthor(`Botadı`, client.user.avatarURL) 
       .setDescription('**[Website]() [destek sunucumuz]() [oyver]()**')
       .setThumbnail(client.user.avatarURL)
-      .addField('`ban`,`bankaldır`,`banlimit`,`bansorgulama`')
+      .addField('** !yardım1 komut adı komut hakkında bilgi**','`ban`,`bankaldır`,`banlimit`,`bansorgulama`')
       .addField('`ust`,`uyar`,`uyarı-kaldır`,`uyarı-sayı`,`!reklamtaraması`,`!yavaş-mod`')
       .addField(' `güvenlik`,`güvenlik-sıfırla`,`sunucu-koruma`,`giriş-izni`')
   .setFooter(``, client.user.avatarURL)
     .setTimestamp()
     message.channel.send(juke).catch()
+  
 
 };
 
