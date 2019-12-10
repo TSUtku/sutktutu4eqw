d3e919ee06499ebb681f8e54cff24808d9eec23b
Glitch (hello-express) <none>
2019-12-10 22:01:19 +0000

    🌒🐠 Checkpoint ./komutlar/öp.js:5958247/2526:6409107/2 ./watch.json:5958247/2 ./komutlar/eğlence.js:6409107/11

--- komutlar/öp.js
+++ komutlar/öp.js
@@ -1,30 +1,39 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 let ayarlar = require("../ayarlar.json");
-let prefix = ayarlar.prefix
+let prefix = ayarlar.prefix;
 
 exports.run = function(client, msg, author) {
-let yazi msg.mentions.members.first();
-if (!yazi) {
-msg.channel.sendMessage(
-"**Örnek Kullanım:** `" +
-prefix +
-"!öp @kullanıcı"
-);
-}
-if (yazi) {
-const embed = Discord.RichEmbed()
-.setTitle("bot adı")
-.setDescripton(
-`wooaw ❤ ** <@${msg.authot.id}> Adlı kullanıcı ${yazi} Adlı kullanıcıyı öptü!**`
-)
-.setColor("BLUE")
-.setImage(
-"https://cdn.discordapp.com/attachments/634056817017880586/645300230790840360/tenor-1.gif"
-);
-msg.channel.sendMessage(embed);
-}
+  let yazi = msg.mentions.members.first();
+  if (!yazi) {
+    msg.channel.sendMessage(
+      "**Örnek Kullanım:** `" +
+        prefix +
+        "öp <@kullanıcı>`"
+    );
+  }
+  if (yazi) {
+    const Embed = new Discord.RichEmbed()
+      .setTitle("")
+      .setDescription(
+        `Vooov ❤ ** <@${msg.author.id}> Adlı Kullanıcı ${yazi} Adlı Kullanıcıyı Öptü!**`
+      )
+      .setColor("RANDOM")
+      .setImage(
+        "https://cdn.discordapp.com/attachments/634056817017880586/645300230790840360/tenor-1.gif"
+      );
+    msg.channel.sendMessage(Embed);
+  }
 };
 
-exports.conf => {
-guildOnly: false,
-}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'öp',
+  description: 'Bu komut ile istediğiniz kişiye öpersin!',
+  usage: 'öp <@kullanıcı'
+};
