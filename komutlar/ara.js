6d6053c49456673c7adf849aaf5586c5dd1b05a5
Glitch (hello-express) <none>
2019-12-11 21:48:58 +0000

    🍢👯 Checkpoint ./komutlar/ara.js:5958247/6363

--- komutlar/ara.js
+++ komutlar/ara.js
@@ -4,14 +4,15 @@ const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const youtube = new YouTube('AIzaSyCni4JiJtaFvS2xEyyiqgdi6OcLKVtMGwY');
 
-const args = msg.content.split(' ');
+
+	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 	const serverQueue = queue.get(msg.guild.id);
 exports.run = async (client, message, args) => {
 	let command = msg.content.toLowerCase().split(' ')[0];
 
-	if (command === 'çal') {
+	 {
 		const voiceChannel = msg.member.voiceChannel;
 		if (!voiceChannel) return msg.channel.sendEmbed(new Discord.RichEmbed()
       .setColor('RANDOM')
@@ -35,17 +36,16 @@ exports.run = async (client, message, args) => {
 				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
 				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
 			}
-    	 return msg.channel.sendEmbed(new Discord.RichEmbed)
-      .setTitle(`✅** | **${playlist.title}** Adlı Şarkı Kuyruğa Eklendi!**`)
+			 return msg.channel.sendEmbed(new Discord.RichEmbed)
+    
 		} else {
 			try {
 				var video = await youtube.getVideo(url);
 			} catch (error) {
 				try {
 					var videos = await youtube.searchVideos(searchString, 10);
-					let index = 0;  
-   
-
+					let index = 0;
+          
 				 msg.channel.sendEmbed(new Discord.RichEmbed()                  
          .setTitle('Şarkı Seçimi')
          .setDescription(`${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}`)
@@ -64,8 +64,21 @@ exports.run = async (client, message, args) => {
 						 return msg.channel.sendEmbed(new Discord.RichEmbed()
             .setColor('0x36393E')
             .setDescription('❎ | **10 Saniye İçinde Şarkı Seçmediğiniz İçin seçim İptal Edilmiştir!**.'));
-          }
-                    };
+                    }
+					const videoIndex = parseInt(response.first().content);
+					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+				} catch (err) {
+					console.error(err);
+					return msg.channel.sendEmbed(new Discord.RichEmbed()
+          .setColor('0x36393E')
+          .setDescription('❎ | YouTubede Böyle Bir Şarkı Yok !**'));
+                }
+            }
+ }
+   
+   };
+  
+                   
 exports.conf = {
     enabled: true,
     aliases: ['çal', 'play'],
