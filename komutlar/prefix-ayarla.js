5014da5cef6ad864314bad06c0da6410b3cd5cf9
Glitch (hello-express) <none>
2019-12-12 22:35:02 +0000

    🍗🛣 Checkpoint ./komutlar/prefix-ayarla.js:5958247/7795 ./events/message.js:5958247/1644 ./watch.json:5958247/10 ./bot.js:5958247/1203

--- komutlar/prefix-ayarla.js
+++ komutlar/prefix-ayarla.js
@@ -1,36 +1,39 @@
 const Discord = require("discord.js");
-const fs = require("fs");
+const db = require('quick.db');
+const ayarlar = require('../ayarlar.json');
 
-module.exports.run = async (bot, message, args) => {
+exports.run = async (client, message, args) => {
+if (!message.guild) {
+  return message.author.send(`:x: Bu komut özel mesajlarda kullanıma kapatılmıştır.`); }
+if (message.author.bot === true) {
+  return;
+}
 
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(":negative_squared_cross_mark: Bunu Yapmak İçin Gerekli Yetkiye Sahip Değilsin!");
-  if(!args[0] || args[0 == "yardım"]) return message.reply("Kullanımı: `!prefix Yeni Ön-Ek`");
-
-  let prefixes = JSON.parse(fs.readFileSync("./sunucuyaözelayarlar/prefix.json", "utf8"));
-
-  prefixes[message.guild.id] = {
-    prefixes: args[0]
-  };
-
-  fs.writeFile("./sunucuyaözelayarlar/prefix.json", JSON.stringify(prefixes), (err) => {
-    if (err) console.log(err)
-  });
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için \`Yönetici\` iznine sahip olmalısın!`);
+  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
+  let nemesis = args.slice(0).join(' ');
 
   
-  message.channel.send(`Ön-Ek başarıyla \`${args[0]}\` olarak ayarlandı!`)
-
+  if (nemesis === "prefix-sıfırla") {
+   db.delete(`prefix_${message.guild.id}`)
+    message.channel.send(`Bu sunucuya ayarlanmış olan prefix başarıyla \`${ayarlar.prefix}\` olarak değiştirildi. Artık bu prefix ile çalışacağım.`)
+    return;
+  }
+  
+  var yeniprefix = db.set(`prefix_${message.guild.id}`, nemesis)
+    message.channel.send(`Prefix'im başarıyla değişti! Artık \`${yeniprefix}\` prefix'i ile çalışacağım.`)
+    
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
-  permLevel: 3
+  aliases: ['prefiks', 'yeniprefix', 'yeni-prefix', 'ön-ek', 'önek', 'prefix-ayarla', 'prefixayarla', 'önekayarla', 'ön-ek-ayarla'],
+  permLevel: 0
 };
 
 exports.help = {
   name: 'prefix-ayarla',
-  description: 'Sunucunuza özel prefixinizi seçersiniz.',
-  usage: 'prefix-ayarla'
+  description: 'Botun bulunulan sunucuya özel prefix\'ini değiştirirsiniz.',
+  usage: 'prefix <prefix>'
 };
-   
