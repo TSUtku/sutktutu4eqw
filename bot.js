1112465563b4651d047e0bbcbd26d89c9db3b584
Glitch (hello-express) <none>
2019-12-14 14:10:37 +0000

    💫🚴 Checkpoint ./bot.js:5958247/865 ./watch.json:5958247/2

--- bot.js
+++ bot.js
@@ -497,24 +497,5 @@ const invites = {};
     member.guild.channels.get(memberChannel).send(`**${member.user.tag}** Katıldı davet eden: **${inviter.tag}** Daveti kullanan kişi sayısı: **${invite.uses}**`);
   });
 })
-////gelen giden
-client.on('guildMemberAdd', member => {
-  let stark = client.channels.get(db.fetch(`starkcode_${member.guild.id}`))
-  const embed = new Discord.RichEmbed()
-  .setTitle(client.users.get(member.id).username)
-  .setDescription("Hoşgeldin, seninle birlikte "+member.guild.memberCount+" kişiyiz!")
-  .setThumbnail(member.user.avatarURL || member.user.defaultAvatarURL)
-  .setFooter("Nemesisw",member.guild.iconURL)
-  stark.send(embed)
-})
 
-client.on('guildMemberRemove', member => {
-   let knl = client.channels.get(db.fetch(`starkcode_${member.guild.id}`))
-  const embed = new Discord.RichEmbed()
-  .setTitle(client.users.get(member.id).username)
-  .setDescription("Görüşürüz, sensiz "+member.guild.memberCount+" kişiyiz!")
-  .setThumbnail(member.user.avatarURL || member.user.defaultAvatarURL)
-  .setFooter("s",member.guild.iconURL)
-  knl.send(embed)
-})
 client.login(ayarlar.token);
