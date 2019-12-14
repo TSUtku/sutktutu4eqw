26a4004a6d5597d1039be17ec14ebb505d12a2c6
Glitch (hello-express) <none>
2019-12-14 14:22:36 +0000

    🚈💎 Checkpoint ./komutlar/özelkomutsil.js:5958247/2524 ./komutlar/özelkomutliste.js:5958247/1183 ./komutlar/özel-komut-ekle.js:5958247/4710 ./watch.json:5958247/2 ./komutlar/özelkomutyardım.js:5958247/1105

---
+++ komutlar/özelkomutyardım.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('BLUE')
+.setTitle('Özel Komut')
+.setTimestamp()
+.setDescription('!yardımkomut komutismi yazarak komutların ne işe yaradığını bulabilirsiniz')
+.addField('!özel-komut-ekle','özel komut eklersiniz. örnek: kullanıcı: sa bot: as')
+.addField('!kayıtkanal','kayıt kanalını ayarlarsınız kayıt-ol komutu sadece orda çalışır.')
+.addField('!kayıt-rol-ayarla','kayıt-ol komutunu kullanınıca verilecek rol')
+.addField('!kayıt-ol','kayıt olma komutu kayıtsistemi açık değilken kullanılmaz.')
+.setFooter('2019 Nemesis Özel komut Sistemi.')
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'kayıtyardım',
+  description: 'kayıt komutlarını gösterir.',
+  usage: 'kayıtyardım'
+};
