0438d30d0bda360c34775f89bd76f22d8c795124
Glitch (nemesis-bot) <none>
2019-12-22 14:59:32 +0000

    🦃🙍 Checkpoint ./komutlar/hazırsunucu.js:5958247/5

--- komutlar/hazırsunucu.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message) => {
-
-const EndlessLove = new Discord.RichEmbed()
-.setColor('RANDOM')
-.setThumbnail(client.user.avatarURL)
-.setTitle('Nemesis Bot Sunucu Kurma Komutları!!')
-}
